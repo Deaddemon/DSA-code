3adafff7b6a8228c46abaa50848aecfb147c8f83
vinita <77224604+Deaddemon@users.noreply.github.com>
2022-09-01 19:10:36 +0530

    lower_bound

---
+++ learning_through_prblms/Card-constructions.cpp
@@ -0,0 +1,9 @@
+//problem: https://codeforces.com/problemset/problem/1345/B
+//sol: https://codeforces.com/contest/1345/submission/169900556
+
+
+//resources: https://www.geeksforgeeks.org/lower_bound-in-cpp/
+
+//works on sorted array
+//The lower_bound() method in C++ is used to return an iterator pointing to the first element in the range [first, last) which has a value not less than val
+//The number of comparisons performed is logarithmic. Therefore, the time complexity of the above approach is O(logN),
