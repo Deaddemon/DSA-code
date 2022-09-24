04c5c277a803c12521737f20d718798e25ef87a6
vinita <77224604+Deaddemon@users.noreply.github.com>
2022-09-24 23:33:18 +0530

    monotonic-queue

---
+++ learning_through_prblms/sliding_window_maximum.cpp
@@ -0,0 +1,44 @@
+//monotonic queue 
+//A monotonic Queue is a data structure the elements from the front to the end is strictly either increasing or decreasing.
+
+//resources:
+//https://abitofcs.blogspot.com/2014/11/data-structure-sliding-window-minimum.html
+//https://www.youtube.com/watch?v=CZQGRp93K4k
+
+//ques: https://leetcode.com/problems/sliding-window-maximum/
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        deque<int> q;
+        vector<int> ans;
+        
+        for(int i=0 ; i<nums.size() ; i++){
+            
+            
+            if(!q.empty() && q.front() == i-k){
+                q.pop_front();
+            }
+            
+            while(!q.empty() && nums[q.back()]  < nums[i]){
+                q.pop_back();
+            }
+            
+            q.push_back(i);
+            
+            if(i>=k-1){
+                ans.push_back(nums[q.front()]);
+            }
+    
+        }
+        
+        return ans;
+        
+    }
+};
