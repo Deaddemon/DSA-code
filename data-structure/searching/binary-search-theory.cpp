54d96c591cf702b8f5c3e4a70e639348d9d346f9
vinita <77224604+Deaddemon@users.noreply.github.com>
2022-10-24 01:28:59 +0530

    Create binary-search-theory.cpp

---
+++ data-structure/searching/binary-search-theory.cpp
@@ -0,0 +1,10 @@
+ // binary search is the algorithm which is
+ // applicable to 
+ // monotonic functions: whihc maintains a given order
+//  
+ // example :
+ // increasing 
+ // decreasing 
+ // x square vala function
+ // predicate function: functions that return a single TRUE or FALSE
+//   
