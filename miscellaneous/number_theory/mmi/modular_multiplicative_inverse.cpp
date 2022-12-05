d410fbc629756489bad5afe00cc44c2b213274c1
vinita <77224604+Deaddemon@users.noreply.github.com>
2022-12-06 00:22:35 +0530

    Create modular_multiplicative_inverse.cpp

---
+++ miscellaneous/number_theory/mmi/modular_multiplicative_inverse.cpp
@@ -0,0 +1,7 @@
+// (a/b) % m  = ( (a%m) * (b^(-1) %m) ) %m
+//here   b^(-1) %m   is  modular multiplicative inverse of b
+
+//fun to find mmi of a
+//binexp(a , m-2 , m) 
+// bcs using fermets theorem 
+// a^(m-2) % m = a^(-1)
