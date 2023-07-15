db6d457b79ad85244cf6d11806ec3ab2645fc682
vinita <77224604+Deaddemon@users.noreply.github.com>
2023-07-15 15:52:12 +0530

    Create radixSort-bucketSort.cpp

---
+++ data-structure/sorting/radixSort-bucketSort.cpp
@@ -0,0 +1,40 @@
+ 
+ **Resources**
+ 
+https://www.youtube.com/watch?v=JMlYkE8hGJM
+https://www.geeksforgeeks.org/radix-sort-vs-bucket-sort/
+----------------------------------------------------------------------------
+ 
+// number of passes == number of digits in the greatest number
+// If we are using decimal numbers, we will have 10 buckets. If we are using the same for alphabets, we will have 26 buckets.
+
+----------------------------------------------------------------------------
+
+ 
+Time Complexity	
+  Radix Sort:
+  O (d*(n+b)) d = number of digits, n = number of array elements, b = base	
+
+
+  Bucket Sort:
+  In the worst-case scenario, O(n+k)
+  [If we use a linked list to represent every element inside the bucket, then it will be O (n*n)].
+ 
+----------------------------------------------------------------------------
+
+ 
+----------------------------------------------------------------------------
+ 
+
+
+----------------------------------------------------------------------------
+ 
+
+
+
+----------------------------------------------------------------------------
+
+
+
+
+---------------------------------------------------------------------------- 
